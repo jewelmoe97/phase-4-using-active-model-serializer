6c2b679c24cb0077e082553e6e8077e950a53ed2
jewel <snowdecember079@gmail.com>
2023-01-29 20:43:29 -0500

    pass

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
 
   def index
     movies = Movie.all
@@ -10,7 +14,10 @@ class MoviesController < ApplicationController
     movie = Movie.find(params[:id])
     render json: movie
   end
-
+def summaries
+  movies = Movie.all
+  render json: movies, each_serializer: MovieSummarySerializer
+end
   private
 
   def render_not_found_response
